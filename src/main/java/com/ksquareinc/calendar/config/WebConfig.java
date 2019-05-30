d719b07c82f69f67a0915b38ce3de985139bc5ba
Roberto Gil <roberto.gil@ksquareinc.com>
2019-05-30 12:33:42 -0500

    Initial commit Added Project generic structure.

---
+++ src/main/java/com/ksquareinc/calendar/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.ksquareinc.calendar.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.ksquareinc.calendar.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
