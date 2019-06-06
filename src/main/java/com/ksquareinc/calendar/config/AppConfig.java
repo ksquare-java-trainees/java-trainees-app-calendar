80a8a25d15d386b34039171b3d67f840bc98bd82
VaroalKs <alvaro.carrillo@ksquareinc.com>
2019-06-06 15:31:07 -0500

    minor changes in db.properties and AppConfig

--- src/main/java/com/ksquareinc/calendar/config/AppConfig.java
+++ src/main/java/com/ksquareinc/calendar/config/AppConfig.java
@@ -3,10 +3,12 @@ package com.ksquareinc.calendar.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
 import java.util.Properties;
 
 import static org.hibernate.cfg.AvailableSettings.*;
@@ -20,17 +22,30 @@ public class AppConfig {
 
     @Autowired
     private Environment env;
+
     @Bean
     public LocalSessionFactoryBean getSessionFactory() {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
 
-        Properties props = new Properties();
-        // Setting JDBC properties
-        props.put(DRIVER, env.getProperty("postgresql.driver"));
-        props.put(URL, env.getProperty("postgresql.url"));
-        props.put(USER, env.getProperty("postgresql.user"));
-        props.put(PASS, env.getProperty("postgresql.password"));
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan("com.ksquare.calendar.model");
+        factoryBean.setHibernateProperties(hibernateProperties());
 
+        return factoryBean;
+    }
+
+    private DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("jdbc.driver"));
+        dataSource.setUrl(env.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(env.getRequiredProperty("jdbc.user"));
+        dataSource.setPassword(env.getRequiredProperty("jdbc.password"));
+
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties props = new Properties();
         // Setting Hibernate properties
         props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
         props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
@@ -41,11 +56,6 @@ public class AppConfig {
         props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
         props.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout"));
         props.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
-
-        factoryBean.setHibernateProperties(props);
-        factoryBean.setPackagesToScan("com.ksquareinc.calendar.model");
-
-        return factoryBean;
     }
 
     @Bean
